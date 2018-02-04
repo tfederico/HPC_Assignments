32cc76a1839168f8e37965466f4ae4de2aa7a462
federico <federicotavella.7@gmail.com>
2018-02-04 18:49:41 +0100

    Complete assignment 3

--- Assignment 3 - GPU computing/res/vector-add/vector-add-kernel.cu
+++
@@ -1,6 +0,0 @@
-__global__ void vec_add_kernel(float *c, float *a, float *b, int n) {
-    int i = 0;   // Oops! Something is not right here, please fix it!
-    if (i < n) {
-        c[i] = a[i] + b[i];
-    }
-}
